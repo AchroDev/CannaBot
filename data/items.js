ea177ed89e85c1fdfa003c6ca433d0d145cb3df4
AchroDev <achrodev@proton.me>
2025-07-04 11:58:55 -0500

    Updated to use products

--- data/items.js
+++ data/items.js
@@ -1,10 +1,12 @@
 const seeds = require('./seeds');
 const nutrients = require('./nutrients');
+const products = require('./products')
 
 // Combine all items into a single Map for easy lookups by ID
 const allItems = new Map();
 
 Object.values(seeds).forEach(item => allItems.set(item.id, item));
 Object.values(nutrients).forEach(item => allItems.set(item.id, item));
+Object.values(products).forEach(item => allItems.set(item.id, item)); 
 
 module.exports = allItems;
