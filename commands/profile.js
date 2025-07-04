e576ecb76de4c9141ea711c1b700f85bcdf17f3a
AchroDev <achrodev@proton.me>
2025-07-04 03:20:34 -0500

    Fixed missing import

--- commands/profile.js
+++ commands/profile.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, MessageFlags } = require('discord.js');
 const User = require('../models/User'); // Import the User model
 
 module.exports = {
