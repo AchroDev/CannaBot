d0d5aa511205bddc8044207b5a79a14e0c95dc6e
AchroDev <achrodev@proton.me>
2025-07-04 11:58:44 -0500

    Created products for defining items to get from harvesting

---
+++ data/products.js
@@ -0,0 +1,24 @@
+module.exports = {
+    og_kush_bud: {
+        id: "og_kush_bud",
+        type: "product",
+        name: "OG Kush Bud",
+        description: "A harvested bud of OG Kush.",
+        // No `price` because you can't buy it, only `sellPrice`.
+        sellPrice: 15 
+    },
+    blue_dream_bud: {
+        id: "blue_dream_bud",
+        type: "product",
+        name: "Blue Dream Bud",
+        description: "A harvested bud of Blue Dream.",
+        sellPrice: 40
+    },
+    freedom_bloom_bud: {
+        id: "freedom_bloom_bud",
+        type: "product",
+        name: "Freedom Bloom Bud",
+        description: "A harvested bud of the festive Freedom Bloom.",
+        sellPrice: 60 
+    },
+};
